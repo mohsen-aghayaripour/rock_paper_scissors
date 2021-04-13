fe3d3980d05820e0938329ea49b9010c9d69a4de
mohsen-aghayaripour <mohsen.aghayaripour@gmail.com>
2021-04-13 22:50:21 +0200

    new function in js file

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,2 +1,21 @@
-console.log("Hello World")
+function computerPlay(items){
+    return items[Math.floor(Math.random() * items.length)];
+     
+}
 
+function playGame(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "paper" || playerSelection === "scissors" && computerSelection === "scissors"){
+        return "Equal, nobody win, try again!"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+    else {
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
+let game = ["rock", "paper", "scissors"];
+let playerInput = prompt("inter your choice :", "rock");
+
+console.log(playGame(playerInput, computerPlay(game)));
