432b079c95b2b1a0ce3702b81020dc3aeec620e4
mohsen-aghayaripour <mohsen.aghayaripour@gmail.com>
2021-04-13 23:05:55 +0200

    did some change in js file

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -5,11 +5,13 @@ function computerPlay(items){
 
 function playGame(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === "rock" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "paper" || playerSelection === "scissors" && computerSelection === "scissors"){
-        return "Equal, nobody win, try again!"
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
+
+    if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
         return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+
+    else if (playerSelection == computerSelection){
+        return "it is tie! You chose " + playerSelection + " and com. chose " + computerSelection
     }
     else {
         return "You Lose! " + computerSelection + " beats " + playerSelection;
